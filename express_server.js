4754d4f4d1222e974f6c813fbb9dd350a3b90191
Mohamed Hassan <mhass405@gmail.com>
2021-04-13 18:48:25 +0000

    Using post to delete URLs

--- express_server.js
+++ express_server.js
@@ -19,6 +19,7 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// not functional this is just to print urlDatabase
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -37,12 +38,14 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
+  
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;  // Log the POST request body to the console
+  
   console.log(urlDatabase);
-  // console.log(req.body);
   res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
 });
+
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
@@ -52,6 +55,13 @@ app.get("/u/:shortURL", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase.shortURL };
   res.render('urls_show', templateVars);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  
+  delete urlDatabase[req.params.shortURL];
+  console.log(urlDatabase);
+  res.redirect(`/urls/`);
 });
 
 app.listen(PORT, () => {
