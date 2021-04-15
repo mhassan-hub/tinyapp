84e70e42c3fe7b4e3fa6705fa1d511983a94bd10
Mohamed Hassan <mhass405@gmail.com>
2021-04-15 18:24:12 +0000

    Providing access to creating new urls is now only reserved to logged in users

--- express_server.js
+++ express_server.js
@@ -3,65 +3,109 @@ const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
+const {generateRandomString, getUserIdByEmail} = require('./Helpers/helper_functions');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser())
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
-const generateRandomString = function() {
-  return Math.random().toString(20).substr(2, 6);
-};
+// const generateRandomString = function() {
+//   return Math.random().toString(20).substr(2, 6);
+// };
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+// const getUserIdByEmail = function (email, users) {
+//   for (let id in users) {
+//     if (email === users[id].email) {
+//       return id;
+//     }
+//   }
+// };
+
+const urlsForUserID = function (email, users, urlDatabase) {
+  const result = {};
+  const id = getUserIdByEmail(email, users);
+
+  for (const url in urlDatabase) {
+    if (url.userID === email) {
+      result[url] = urlDatabase[url];
+    }
+  }
+  return result;
 };
 
 const users = {
   "userRandomID": {
     id: "userRandomID", 
     email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
+    password: "hi"
   },
- "user2RandomID": {
+  "user2RandomID": {
     id: "user2RandomID", 
     email: "user2@example.com", 
-    password: "dishwasher-funk"
+    password: "no"
+  },
+  "aJ48lW": {
+    id: "aJ48lW", 
+    email: "mhass405@gmail.com", 
+    password: "as"
   }
 };
 
-const getId = function (email, users) {
-  for (let id in users) {
-    if (email === users[id].email) {
-      return id;
-    }
+const urlDatabase = {
+  "b2xVn2": { 
+    longURL: "http://www.lighthouselabs.ca", 
+    userID: "aJ48lW"
+  },
+  "9sm5xK": { 
+    longURL: "http://www.google.com", 
+    userID: "aJ48lW"
   }
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Welcome to TinyApp V15.2.4! We are here to supply you with al your link shortening needs!");
 });
 
+/*-----------/URLS/-------------*/
+
 app.get("/urls", (req, res) => {
+  const userURL = urlsForUserID(req.cookies["user_id"], users, urlDatabase)
+
   const templateVars = { 
-    urls: urlDatabase, 
+    urls: userURL, 
     user: users[req.cookies.users_id]
   };
+
+  console.log(users[req.cookies["user_id"]]);
   res.render('urls_index', templateVars);
 });
 
 app.post("/urls", (req, res) => {
   
   let shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
-  // console.log(urlDatabase);
+
+
+  urlDatabase[shortURL] = {
+    longURL: req.body.longURL,
+    userID: req.cookies.users_id
+  };
+
   res.redirect(`/urls/${shortURL}`);
 });
 
+/*-----------/URLS/NEW------------*/
+
 app.get("/urls/new", (req, res) => {
+
+  if (!req.cookies.users_id) {
+    res.redirect("/login");
+  };
+
   const templateVars = {user: users[req.cookies.users_id]};
   res.render("urls_new", templateVars);
 });
+
+/*-----------/URLS/LOGIN------------*/
 
 app.get("/login", (req, res) => {
   const templateVars = {user: users[req.cookies.users_id]};
@@ -70,29 +114,36 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
   // res.redirect("/login");
-  let email = req.body.email
+  let email = req.body.email;
   let password = req.body.password;
 
-  let id = getId(email, users)
-  // console.log(users[id].password);
+  let id = getUserIdByEmail(email, users);
+
   if (id && password === users[id].password) {
     res.cookie("users_id", id);
     res.redirect("/urls");
-  }
+  };
+
   res.statusCode = 403;
   return res.send(`${res.statusCode}: please enter the correct email and password`)
 });
 
+/*-----------/URLS/LOGOUT------------*/
+
 app.post("/logout", (req, res) => {
-  res.clearCookie("users_id")
-  res.redirect("/urls")
+
+  res.clearCookie("users_id");
+
+  res.redirect("/urls");
 });
+
+/*-----------/URLS/REGISTER------------*/
 
 app.get("/register", (req, res) => {
   
   const templateVars = { user: users[req.cookies.users_id] };
 
-  res.render("urls_register", templateVars)
+  res.render("urls_register", templateVars);
 });
 
 app.post("/register", (req, res) => {
@@ -103,17 +154,18 @@ app.post("/register", (req, res) => {
   let password = req.body.password;
 
   if (email === '' || password === '') {
-    
-    console.log('hello');
+
     res.statusCode = 400;
-    return res.send(`${res.statusCode}: This email already exists`)
-  }
-  users[id] = {id, email, password}
-  // user = {id, email, password}
+    return res.send(`${res.statusCode}: This email already exists`);
+  };
+
+  users[id] = {id, email, password};
+
   res.cookie("users_id", id);
-  console.log(users);
   res.redirect(`/urls`);
 });
+
+/*-----------/URLS/SHORTURL'S------------*/
 
 app.get("/urls/:shortURL", (req, res) => {
   
@@ -121,27 +173,36 @@ app.get("/urls/:shortURL", (req, res) => {
   
   const templateVars = { 
     shortURL, 
-    longURL: urlDatabase[shortURL], 
+    longURL: urlDatabase[shortURL].longURL, 
     user: users[req.cookies.users_id] 
   };
+  console.log(urlDatabase[req.params.shortURL].longURL);
   res.render('urls_show', templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL].longURL;
+  res.redirect(longURL);
 });
 
 app.post("/urls/:shortURL", (req, res) => {
   
-  urlDatabase[req.params.shortURL] = req.body.longURL;
+  urlDatabase[req.params.shortURL].longURL = req.body.longURL;
+
   res.redirect(`/urls/${req.params.shortURL}`);
 });
+
+/*-------------/URLS/SHORTURL/DELETE-------------*/
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   
   delete urlDatabase[req.params.shortURL];
 
-  console.log(urlDatabase);
   res.redirect(`/urls/`);
 });
 
+/*-----------LISTENING TO PORT 8080------------*/
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`TinyApp V15.2.4 is listening on port ${PORT}!`);
 });
